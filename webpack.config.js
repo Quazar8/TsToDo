944854cf2267f818cfb73cdb2f5d24812730502f
Quazar8 <barcode12d@gmail.com>
2020-10-31 21:52:03 +0200

    Setting up webpack and typescript
    Configuring webpack.config.js and tsconfig.json
    to work with typescript and html-webpack-plugin.

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry:"./src/index.ts",
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve:{
+        extensions:['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules:[
+            {
+                test:/\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
