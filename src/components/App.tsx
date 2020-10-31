1a97c1d3e39da66de97127ff5794fb7d41a51b2c
Quazar8 <barcode12d@gmail.com>
2020-10-31 23:56:40 +0200

    Adding a container view for all ToDos

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import Input from './Input'
+import ToDosContainer from './ToDosContainer'
 
 const App = props => {
     return (
         <main className="app-container">
             <h1>Hello from React</h1>
             <Input />
+            <ToDosContainer />
         </main>
     )
 }
---
+++ src/components/ToDosContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ToDosContainer = props => {
+    return (
+        <section>
+            ToDos Container
+        </section>
+    )
+}
+
+export default ToDosContainer
