e1f80e7d5a2c825271c3af73387c729fd250b3c7
Quazar8 <barcode12d@gmail.com>
2020-10-31 23:49:54 +0200

    Attaching an "Add" button to the Input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,6 +4,7 @@ const Input = props => {
     return (
         <section className="input-container">
             <input type="text" />
+            <input type="submit" value="Add" />
         </section>
     )
 }
