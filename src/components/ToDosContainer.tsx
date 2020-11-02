b754f473ffb05ed2ee271fa883d3eaeab1de04b7
Quazar8 <barcode12d@gmail.com>
2020-11-02 14:58:41 +0200

    More types and interfaces for ToDosContainer

--- src/components/ToDosContainer.tsx
+++ src/components/ToDosContainer.tsx
@@ -2,17 +2,30 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { IState } from '../store/store'
 
-const ToDosContainerView = ({ todos }) => {
+interface IToDosContainerState {
+    todos: string[]
+}
+
+const ToDosContainerView = 
+    ({ todos }): JSX.Element => {
+
+    const renderTodos = (todo: string): JSX.Element => {
+        return (
+            <div>{todo}</div>
+        )
+    }
     return (
         <section>
-            { todos.map(todo => (
-                <div>{todo}</div>
-            ))}
+            {todos.length > 0
+             ? todos.map(renderTodos)
+             : <h1>No todos yet.</h1>
+            }
         </section>
     )
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = 
+    (state: IState): IToDosContainerState => {
     return {
         todos: state.todos
     }
